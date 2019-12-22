5fe589b8fb4999dd5a46f165e12a97f67ac4e699
tereshchenkomax <tereshchenkomax@gmail.com>
2019-12-22 19:21:56 +0200

    collab-react-components server

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const CollabServer = require('collab-react-components').Server;
+const CollabCollection = require('collab-react-components').Collection;
 mongoose.set('useFindAndModify', false);
 
 mongoose.Promise = Promise;
@@ -16,6 +18,14 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
+const options = {
+	db: {
+		type: 'mongo',
+		url
+	}
+};
+
+CollabServer.start(app, options);
 app.use(express.json({limit: '50mb'}));
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
@@ -23,5 +33,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/', indexRouter);
+
+// Create the collection that will hold the shared data.
+const documents = new CollabCollection('documents');
+
+// Create the shared form data
+documents.createRichText('rich-editor1', 'My initial data');
 
 module.exports = app;
